0240a62ecf0770374b31823002653fd79debdcd0
bruxie <bagneguechristb@gmail.com>
2026-01-15 21:30:16 +0100

    Mise à jour de la sécurité,implementation du code des pages introuvable 404 , et affichage du prenom de la personne connecté

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -30,8 +30,9 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // do anything else you need here, like send an email
-            return $this->redirectToRoute('app_home');
+            // Rediriger vers la page de connexion après inscription
+            $this->addFlash('success', 'Votre compte a été créé avec succès ! Vous pouvez maintenant vous connecter.');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
